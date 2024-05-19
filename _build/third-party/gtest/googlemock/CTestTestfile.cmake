4cbda076143c4fa1fd6d716dc717ba1fd2b2203f
3Wc4YEM5 <aibushev.goscha@yandex.ru>
2024-05-19 17:38:09 +0300

    CI3

--- _build/third-party/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/georgiy/3Wc4YEM5/RK2/third-party/gtest/googlemock
-# Build directory: /home/georgiy/3Wc4YEM5/RK2/_build/third-party/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
